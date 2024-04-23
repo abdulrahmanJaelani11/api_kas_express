2a667544fde7b4f5bbfc42c3ff1876d0e6d3990d
Abdurahman Jaelani <randikaangga9044@gmail.com>
2024-04-23 13:36:32 +0700

    mengubah konksi db

--- database.js
+++ database.js
@@ -5,16 +5,17 @@ const db = mysql.createConnection({
   // password: "",
   // database: "kas_app",
 
-  // host: "sql311.infinityfree.com",
-  // user: "if0_36013679",
-  // password: "bA1hCs3J6HoO9NL",
-  // database: "if0_36013679_kas_express",
+  host: "aaj.h.filess.io",
+  database: "sicakas_mustvapor",
+  port: "3307",
+  user: "sicakas_mustvapor",
+  password: "820688fef06604fd1425f7227e6dce4b5fbd5257",
 
-  host: "sql6.freesqldatabase.com",
-  user: "sql6698860",
-  password: "PAtWpNJ51G",
-  database: "sql6698860",
-  port: "3306",
+  // host: "sql6.freesqldatabase.com",
+  // user: "sql6698860",
+  // password: "PAtWpNJ51G",
+  // database: "sql6698860",
+  // port: "3306",
 });
 
 module.exports = db;
