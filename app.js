54279f69955bda8c35de0fa730095a2691d2bcf3
Abdurahman Jaelani <randikaangga9044@gmail.com>
2024-04-24 21:18:25 +0700

    Rubah domain

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://bayarkas.vercel.app"); // Ganti dengan domain Vue.js Anda
+  res.header("Access-Control-Allow-Origin", "https://bayarkas.vercel.app/"); // Ganti dengan domain Vue.js Anda
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
