9f98329b27ec4e09d906331443c9a98d67e4f490
DESKTOP-JHB7MEC\Workplus <randikaangga9044@gmail.com>
2024-05-09 23:13:38 +0800

    menambahkan email dan no telp di anggota

--- database.js
+++ database.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql");
 const db = mysql.createConnection({
-  // host: "localhost",
-  // user: "root",
-  // password: "",
-  // database: "kas_app",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "kas_express",
 
-  host: "aaj.h.filess.io",
-  database: "sicakas_mustvapor",
-  port: "3307",
-  user: "sicakas_mustvapor",
-  password: "820688fef06604fd1425f7227e6dce4b5fbd5257",
+  // host: "aaj.h.filess.io",
+  // database: "sicakas_mustvapor",
+  // port: "3307",
+  // user: "sicakas_mustvapor",
+  // password: "820688fef06604fd1425f7227e6dce4b5fbd5257",
 
   // host: "sql6.freesqldatabase.com",
   // user: "sql6698860",
